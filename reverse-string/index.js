ea24414d39d6507b7a851a12139b7ab587a7813f
Andrew Usher <andrewusher00@gmail.com>
2018-05-16 17:43:47 -0500

    Split if statement into seperate lines for reverse string algorithm

--- reverse-string/index.js
+++ reverse-string/index.js
@@ -1,10 +1,17 @@
 const reverseString = string => {
-  if (typeof string === 'object' || Array.isArray(string) || typeof string === 'number') return false
-  return string
-    .toLowerCase()
-    .split('')
-    .reverse()
-    .join('')
+  if (
+    typeof string === 'object' ||
+    Array.isArray(string) ||
+    typeof string === 'number'
+  ) {
+    return false
+  } else {
+    return string
+      .toLowerCase()
+      .split('')
+      .reverse()
+      .join('')
+  }
 }
 
 module.exports = reverseString
