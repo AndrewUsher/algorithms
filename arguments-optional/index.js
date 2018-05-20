930340de48d98eca7f2a892594a2b15532b2f8ff
Andrew Usher <andrewusher00@gmail.com>
2018-05-20 13:58:55 -0500

    Add description to arguments optional algorithm

--- arguments-optional/index.js
+++ arguments-optional/index.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-return-assign */
+// Create a function that sums two arguments together.
+// If one arg is provided, then return a function that expects one arg and returns the sum.
 const argumentsOptional = () => {
   const args = Array
     .from(arguments)
 
   return args
-    .some(i => typeof i !== 'number') ? undefined : args.length > 1 ? args
-      .reduce((acc, j) => acc += j, 0) : j => typeof j === 'number' ? j + args[0] : undefined
+    .some(i => typeof i !== 'number')
+    ? undefined
+    : args.length > 1
+      ? args.reduce((acc, j) => acc += j, 0)
+      : j => typeof j === 'number'
+        ? j + args[0]
+        : undefined
 }
 
 module.exports = argumentsOptional
