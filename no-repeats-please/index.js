6cd6d17951a21925bd254a4e45249043ff38583c
Andrew Usher <andrewusher00@gmail.com>
2018-05-20 14:00:04 -0500

    Add no repeats please algorithm (from FCC)

---
+++ no-repeats-please/index.js
@@ -0,0 +1,36 @@
+
+const noRepeatsPlease = str => {
+  const regex = /(.)\1+/g
+
+  const arr = str.split('')
+  const permutations = []
+  let tmp
+
+  if (str.match(regex) !== null && str.match(regex)[0] === str) return 0
+
+  const swap = (index1, index2) => {
+    tmp = arr[index1]
+    arr[index1] = arr[index2]
+    arr[index2] = tmp
+  }
+
+  const generate = int => {
+    if (int === 1) {
+      permutations.push(arr.join(''))
+    } else {
+      for (let i = 0; i !== int; ++i) {
+        generate(int - 1)
+        swap(int % 2 ? 0 : i, int - 1)
+      }
+    }
+  }
+
+  generate(arr.length)
+
+  const filtered = permutations
+    .filter(string => !string.match(regex))
+
+  return filtered.length
+}
+
+module.exports = noRepeatsPlease
